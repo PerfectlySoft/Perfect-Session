c602d698f5f92afc1b75a8d524984640565eb88a
Jonathan Guthrie <jono@perfect.org>
2017-01-02 19:07:10 -0500

    Update to HTTP 2.1

--- Package.swift
+++ Package.swift
@@ -12,6 +12,6 @@ let package = Package(
 		],
     dependencies: [
 		.Package(url: "https://github.com/PerfectlySoft/PerfectLib.git", majorVersion: 2, minor: 0),
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTP.git", majorVersion: 2, minor: 0),
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTP.git", majorVersion: 2, minor: 1),
     ]
 )
