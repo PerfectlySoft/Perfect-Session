26561d513fc25b1ba1d1e9b0e5711d7fc968191f
Jonathan Guthrie <jono@perfect.org>
2017-03-09 16:34:47 -0500

    adding perfect repeater

--- Package.swift
+++ Package.swift
@@ -8,5 +8,6 @@ let package = Package(
 		.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTP.git", majorVersion: 2),
 		.Package(url: "https://github.com/PerfectlySoft/Perfect-Logger.git", majorVersion: 1),
 		.Package(url: "https://github.com/iamjono/SwiftString.git", majorVersion: 1),
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-Repeater.git", majorVersion: 1),
 	]
 )
