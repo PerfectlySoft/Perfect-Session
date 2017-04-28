873ddb6f9e22ea94fbc966ffa182be57e8b6dfb6
Jonathan Guthrie <jono@perfect.org>
2017-04-28 17:09:39 -0400

    add filter

--- Sources/PerfectSession/AuthFilters.swift
+++ Sources/PerfectSession/AuthFilters.swift
@@ -6,4 +6,86 @@
 //
 //
 
-import Foundation
+import PerfectHTTP
+import SwiftString
+
+
+/// Sets up the matrix of which routes are checked for authentication, and which are not.
+public struct AuthenticationConfig {
+
+	/// An array of routes that are checked for valid authentication.
+	public var inclusions = [String]()
+
+	/// An array of routes that will not be checked.
+	public var exclusions = [String]()
+
+	/// Unimplemented. Will optionally provide a location for access denied redirection.
+	public var denied: String?
+
+	public init() {}
+
+	/// Add a route as a string to the array of inclusions
+	public mutating func include(_ str: String) {
+		inclusions.append(str)
+	}
+	/// Add a an array of strings (as routes) to the array of inclusions
+	public mutating func include(_ arr: [String]) {
+		inclusions += arr
+	}
+	/// Add a route as a string to the array of exclusions
+	public mutating func exclude(_ str: String) {
+		exclusions.append(str)
+	}
+	/// Add a an array of strings (as routes) to the array of exclusions
+	public mutating func exclude(_ arr: [String]) {
+		exclusions += arr
+	}
+}
+
+/// Contains the filtering mechanism for determining valid authentication on routes.
+public struct AuthFilter: HTTPRequestFilter {
+
+	/// The authentication configuration. Holds the routes to be included or excluded
+	var authenticationConfig = AuthenticationConfig()
+
+	/// Accept an auth config via init.
+	public init(_ cfg: AuthenticationConfig) {
+		authenticationConfig = cfg
+	}
+
+	/// Perform the filtering, with a callback allowing continuation of request, or galting immediately.
+	public func filter(request: HTTPRequest, response: HTTPResponse, callback: (HTTPRequestFilterResult) -> ()) {
+
+		//		guard let denied = authenticationConfig.denied else {
+		//			callback(.continue(request, response))
+		//			return
+		//		}
+		var checkAuth = false
+		let wildcardInclusions = authenticationConfig.inclusions.filter({$0.contains("*")})
+		let wildcardExclusions = authenticationConfig.exclusions.filter({$0.contains("*")})
+
+		// check if specifically in inclusions
+		if authenticationConfig.inclusions.contains(request.path) { checkAuth = true }
+		// check if covered by a wildcard
+		for wInc in wildcardInclusions {
+			if request.path.startsWith(wInc.split("*")[0]) { checkAuth = true }
+		}
+
+		// ignore check if sepecified in exclusions
+		if authenticationConfig.exclusions.contains(request.path) { checkAuth = false }
+		// check if covered by a wildcard
+		for wInc in wildcardExclusions {
+			if request.path.startsWith(wInc.split("*")[0]) { checkAuth = false }
+		}
+
+		if checkAuth && !(request.session?.userid ?? "").isEmpty {
+			callback(.continue(request, response))
+			return
+		} else if checkAuth {
+			response.status = .unauthorized
+			callback(.halt(request, response))
+			return
+		}
+		callback(.continue(request, response))
+	}
+}
