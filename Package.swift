37bbeac3c03262a26744ba6e525cfc6ff627c9de
Jonathan Guthrie <jono@perfect.org>
2017-09-29 14:21:29 -0400

    update to swift 4 / perfect 3

--- Package.swift
+++ Package.swift
@@ -4,10 +4,10 @@ import PackageDescription
 let package = Package(
     name: "PerfectSession",
     dependencies: [
-		.Package(url: "https://github.com/PerfectlySoft/PerfectLib.git", majorVersion: 2),
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTP.git", majorVersion: 2),
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 2),
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-Logger.git", majorVersion: 1),
+		.Package(url: "https://github.com/PerfectlySoft/PerfectLib.git", majorVersion: 3),
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTP.git", majorVersion: 3),
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 3),
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-Logger.git", majorVersion: 3),
 		.Package(url: "https://github.com/iamjono/SwiftString.git", majorVersion: 2),
 		.Package(url: "https://github.com/PerfectlySoft/Perfect-Repeater.git", majorVersion: 1),
 	]
